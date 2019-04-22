09e8e5fef3a21c8258713f45d54726d2e5cf46b3
eppelas <eppelas@gmail.com>
2019-04-22 14:44:12 +0300

    Cart Script Added

--- js/cart.js
+++ js/cart.js
@@ -1,137 +1,433 @@
-/*localStorage.clear();
-*/
-
-
-
-
 var data = {
    "cart": [
+
       ["fiction", "p1pm1", 1],
       ["mix", "p1pm3", 1],
       ["fiction", "p1pm6", 3]
    ],   
-   "boxes": {
-      "fiction": {
+   
+
+   "boxes":{  
+      "fiction":{  
+         "id":"fiction",
          "name":"Художественная",
-         "face_price":900,
-         "subscriptions": {
-            "p1pm1": {
+         "subscriptions":{  
+            "p1pm1":{  
+               "id":"p1pm1",
                "name":"1 месяц",
                "months":1,
                "price":900,
                "boxType":"plain"
-             },
-            "p1pm3": {
+            },
+            "p1pm3":{  
+               "id":"p1pm3",
                "name":"3 месяца",
                "months":3,
                "price":2550,
                "boxType":"plain"
-             },
-            "p1pm6": {
+            },
+            "p1pm6":{  
+               "id":"p1pm6",
                "name":"6 месяцев",
                "months":6,
                "price":4800,
                "boxType":"plain"
-             }
-          }
-       },
-      "mix": {
+            },
+            "p1gm1":{  
+               "id":"p1gm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":1900,
+               "boxType":"gift"
+            },
+            "p1gm3":{  
+               "id":"p1gm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":5250,
+               "boxType":"gift"
+            },
+            "p1gm6":{  
+               "id":"p1gm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":9600,
+               "boxType":"gift"
+            }
+         }
+      },
+      "mix":{  
+         "id":"mix",
          "name":"Микс",
-         "face_price":900,
-         "subscriptions": {
-            "p1pm1": {
+         "subscriptions":{  
+            "p1pm1":{  
+               "id":"p1pm1",
                "name":"1 месяц",
                "months":1,
                "price":900,
                "boxType":"plain"
-             },
-            "p1pm3": {
+            },
+            "p1pm3":{  
+               "id":"p1pm3",
                "name":"3 месяца",
                "months":3,
                "price":2550,
                "boxType":"plain"
-             }
-          }
-       }
-    }
+            },
+            "p1pm6":{  
+               "id":"p1pm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":4800,
+               "boxType":"plain"
+            },
+            "p1gm1":{  
+               "id":"p1gm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":1900,
+               "boxType":"gift"
+            },
+            "p1gm3":{  
+               "id":"p1gm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":5250,
+               "boxType":"gift"
+            },
+            "p1gm6":{  
+               "id":"p1gm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":9600,
+               "boxType":"gift"
+            }
+         }
+      },
+      "classic":{  
+         "id":"classic",
+         "name":"Классика",
+         "subscriptions":{  
+            "p1pm1":{  
+               "id":"p1pm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":900,
+               "boxType":"plain"
+            },
+            "p1pm3":{  
+               "id":"p1pm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":2550,
+               "boxType":"plain"
+            },
+            "p1pm6":{  
+               "id":"p1pm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":4800,
+               "boxType":"plain"
+            },
+            "p1gm1":{  
+               "id":"p1gm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":1900,
+               "boxType":"gift"
+            },
+            "p1gm3":{  
+               "id":"p1gm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":5250,
+               "boxType":"gift"
+            },
+            "p1gm6":{  
+               "id":"p1gm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":9600,
+               "boxType":"gift"
+            }
+         }
+      },
+      "non-fiction":{  
+         "id":"non-fiction",
+         "name":"Нон-фикшн",
+         "subscriptions":{  
+            "p1pm1":{  
+               "id":"p1pm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":900,
+               "boxType":"plain"
+            },
+            "p1pm3":{  
+               "id":"p1pm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":2550,
+               "boxType":"plain"
+            },
+            "p1pm6":{  
+               "id":"p1pm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":4800,
+               "boxType":"plain"
+            },
+            "p1gm1":{  
+               "id":"p1gm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":1900,
+               "boxType":"gift"
+            },
+            "p1gm3":{  
+               "id":"p1gm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":5250,
+               "boxType":"gift"
+            },
+            "p1gm6":{  
+               "id":"p1gm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":9600,
+               "boxType":"gift"
+            }
+         }
+      },
+      "english":{  
+         "id":"english",
+         "name":"English Box",
+         "subscriptions":{  
+            "p2pm1":{  
+               "id":"p2pm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":1000,
+               "boxType":"plain"
+            },
+            "p2pm3":{  
+               "id":"p2pm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":2850,
+               "boxType":"plain"
+            },
+            "p2pm6":{  
+               "id":"p2pm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":5400,
+               "boxType":"plain"
+            },
+            "p2gm1":{  
+               "id":"p2gm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":2000,
+               "boxType":"gift"
+            },
+            "p2gm3":{  
+               "id":"p2gm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":5550,
+               "boxType":"gift"
+            },
+            "p2gm6":{  
+               "id":"p2gm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":10200,
+               "boxType":"gift"
+            }
+         }
+      },
+      "self-improvement":{  
+         "id":"self-improvement",
+         "name":"Саморазвитие и мотивация",
+         "subscriptions":{  
+            "p2pm1":{  
+               "id":"p2pm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":1000,
+               "boxType":"plain"
+            },
+            "p2pm3":{  
+               "id":"p2pm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":2850,
+               "boxType":"plain"
+            },
+            "p2pm6":{  
+               "id":"p2pm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":5400,
+               "boxType":"plain"
+            },
+            "p2gm1":{  
+               "id":"p2gm1",
+               "name":"1 месяц",
+               "months":1,
+               "price":2000,
+               "boxType":"gift"
+            },
+            "p2gm3":{  
+               "id":"p2gm3",
+               "name":"3 месяца",
+               "months":3,
+               "price":5550,
+               "boxType":"gift"
+            },
+            "p2gm6":{  
+               "id":"p2gm6",
+               "name":"6 месяцев",
+               "months":6,
+               "price":10200,
+               "boxType":"gift"
+            }
+         }
+      }
+   }
  };
 
 
 
-
+/*
 function getProducts(success, failure){
-            //request the list of products from the "server"
-            const URL = "https://github.com/eppelas/getyourbook/blob/master/js/products.json";
-            fetch(URL, {
-                method: 'GET',
-                mode: 'cors'
-            })
-            //turns json to js method
-            .then(response=>response.json())
-            .then(success)
-            /*.then(showProducts)*/
+    //request the list of products from the "server"
+    const URL = "https://github.com/eppelas/getyourbook/blob/master/js/products.json";
+    fetch(URL, {
+        method: 'GET',
+        mode: 'cors'
+    })
+    //turns json to js method
+    .then(response=>response.json())
+    .then(success)
+    /*.then(showProducts)
 
-            .catch(failure);
+    .catch(failure);
 
-            /*.catch(err=>{
-                errorMessage(err.message);
-            });*/
-        };
-
+    .catch(err=>{
+        errorMessage(err.message);
+    });
+};
+*/
 
 /*alert(cart);*/
 
 
-/*Storage.prototype.setObject = function(key, value) {
+Storage.prototype.setObject = function(key, value) {
     this.setItem(key, JSON.stringify(value));
 }
 
 Storage.prototype.getObject = function(key) {
     var value = this.getItem(key);
     return value && JSON.parse(value);
-}*/
+}
 
+
+function formatFormData(data){
+ 
+  var code = 'p1'; //p1pm2 || p2jm6 etc. p1 wtf?, p - boxType, m6 - months
+
+
+
+  var boxType = ''; //'p' || 'j'
+  var months = 'm'; // m+number
+
+  var quantity = 0;
+
+  var product = ""; //fiction || mix || whatever
+  
+  // "p1pm3"
+
+  for (var i in data) {
+    if (data[i] && data[i].name == 'product') {
+      product = data[i].value;
+
+    } else if (data[i] && data[i].name == 'months') {
+      months = 'm'+data[i].value;
+
+    } else if (data[i] && data[i].name == 'boxType') {
+      boxType = data[i].value;
+
+    } else if (data[i] && data[i].name == 'quantity') {
+      quantity = data[i].value;
+    }
+  }
+
+  code += boxType + months;
+
+  return [product, code, quantity];
+
+}
+
+
+function addToCart(cur_cart, new_element) {
+
+
+  if (!cur_cart || cur_cart.length == 0) {
+    cur_cart = [];
+    cur_cart.push(new_element);
+
+    return cur_cart;
+  }
+  
+  if (!new_element) {
+    return cur_cart;
+  } 
+
+  var isIncreased = false;
+
+  for (var i in cur_cart){
+
+    console.log(cur_cart[i][0] + "==" + new_element[0], cur_cart[i][0] == new_element[0] )
+    console.log(cur_cart[i][1] + '==' + new_element[1], cur_cart[i][1] == new_element[1])
+
+    if (cur_cart[i][0] == new_element[0] && cur_cart[i][1] == new_element[1]){
+      var quantity = cur_cart[i][2] * 1 + new_element[2] * 1;
+      cur_cart[i][2] = quantity;
+      // if quantity increased - stop checking
+      return cur_cart;
+    }
+  }
+
+  // if quantity was not increased - this is a new element, add it
+  cur_cart.push(new_element);
+  return cur_cart;
+
+}
 
 
 // Событие добавления в корзину
 $('.add_to_cart_button').click(function() {
-   // собираем данные
-   var new_element = ["fiction", "p1pm1", 1];
-   
-   console.log(new_element);
+
+  var $form = $(this).parents('form');
+  var formData = $form.serializeArray();
+
+  var new_element = formatFormData(formData);
 
 
-   // забрать из localStorage что сейчас в корзине
-   var cur_cart = localStorage.getItem('cart');
+  // забрать из localStorage что сейчас в корзине
+  var cur_cart = localStorage.getObject('cart');
 
-   
+  var new_cart = addToCart(cur_cart, new_element);
 
-
-   let new_cart = [];
-   // если есть корзина
-   if (cur_cart) {
-      console.log(cur_cart); 
-      for (var index in cur_cart) {
-        var value = cur_cart[index]
-         // есть ли такое в корзине уже?
-         // добавляем количество
-         if ( (new_element[0] == value[0]) && (new_element[1] == value[1]) ) {
-            new_element[2] += value[2];
-            return new_cart.push(new_element);
-         } else {
-            return new_cart.push(value);
-          }
-      }
-
-   // если нет корзины
-   } else {
-      new_cart = [ new_element ];
-    }
 
    // сохранить в localStorage
-   return localStorage.setItem('cart', new_cart);
+  localStorage.setObject('cart', new_cart);
 
-   createCart(); 
+  renderCart(new_cart);
+
 
 });
 
@@ -142,92 +438,183 @@ $('.add_to_cart_button').click(function() {
 //  document.addEventListener('DOMContentLoaded', ()=>{
   
 
-$(function createCart(i, value) {
-           var box_title = data.boxes[ value[0] ].name;
-           var { months } = data.boxes[ value[0] ].subscriptions[ value[1] ];
-           let lenght = '--';
-           switch (months) {
-              case 1:
-                 lenght = '1 месяц';
-                break;
-              case 3:
-                 lenght = '3 месяца';
-                break;
-              case 6:
-                 lenght = '6 месяцев';
-                break;
-           }
+function createCart(i, value) {
+  var box_title = data.boxes[ value[0] ].name;
+  var { months } = data.boxes[ value[0] ].subscriptions[ value[1] ];
+  let lenght = '--';
+  switch (months) {
+    case 1:
+        lenght = '1 месяц';
+      break;
+    case 3:
+        lenght = '3 месяца';
+      break;
+    case 6:
+        lenght = '6 месяцев';
+      break;
+  }
 
-           var element = `<p> Hello </p> \
-            <p>` + box_title + `</p> \
-            <div class="user_choices"> \
-            <p class="choice_name">Подписка:</p> \
-            <p>` + lenght + `</p> `;
-                    
-           return new_cart_list += element;
-        });
-
-
+  var element = `<p> Hello </p> \
+  <p>` + box_title + `</p> \
+  <div class="user_choices"> \
+  <p class="choice_name">Подписка:</p> \
+  <p>` + lenght + `</p> `;
+          
+  return new_cart_list += element;
+}
 
 
-$(function() { // onload
 
- var cur_cart = localStorage.getItem('cart');
+// [["fiction","p1gm1",26],["fiction","p1pm1",6],["fiction","p1pm6",5]]
+
+
+
+function renderCart(cur_cart) {
+
+  function getMonthsName(m){
+    var length = m;
+
+    m = ''+m;
+    
+    switch (m) {
+      case '1':
+        length = '1 месяц';
+        break;
+      case '3':
+        length = '3 месяца';
+        break;
+      case '6':
+        length = '6 месяцев';
+        break;
+      default:
+        length = `${m} месяцев`;
+        break;
+    }
+    return length
+  }
+
+
+
+
+  function getBoxType(b) {
+    var boxtype = b;
+
+    switch (b) {
+      case 'plain':
+        boxtype = 'Только книги';
+        break;
+      case 'gift':
+        boxtype = 'Стандарт';
+        break;
+    }
+
+    return boxtype
+  }
+
+  function getProductInfo(product_type) {
+
+    return data.boxes[product_type] || data.boxes['fiction'];
+  }
+
+  function getProductSubscription(product_type, subscription) {
+    var product_info = getProductInfo(product_type);
+
+
+    return product_info.subscriptions[subscription] || {}
+  }
+
+
+
+
+  function getLineHtml(rowData) {
+
+    var product_info = getProductInfo(rowData[0]);
+
+    console.log(product_info);
+
+    var product_subscription = getProductSubscription(rowData[0], rowData[1]);
+
+    var product_name = product_info.name;
+    var months = getMonthsName(product_subscription.months);
+    var package = getBoxType(product_subscription.boxType);
+
+    var price = product_subscription.price;
+    var quantity = rowData[2];
+    var total = price * quantity;
+
+
+    rowHtml = `
+      <tr>
+        <td class="description">
+          <h3>${product_name}</h3>
+          <p>Подписка: ${months}</p>
+          <p>Наполнение: ${package}</p>
+        </td>
+        <td class="price">${price}</td>
+        <td class="quantity">${quantity}</td>
+        <td class="total">${total}</td>
+        <td class="remove">
+          <button class="btn btn-link text-danger">X</button>
+        </td>
+      </tr>
+    `;
+
+    return rowHtml;
+    
+  }
+
+  cartHtml = '';
+  for (var i in cur_cart) {
+    cartHtml += getLineHtml(cur_cart[i]);
+  }
+
+  $('.cart-vals').html(cartHtml);
+}
+
+
+
+
+
+
+
+
+ var cur_cart = localStorage.getObject('cart'); 
+
+ renderCart(cur_cart);
 
   // вывод пользователю после обновления корзины
 
 
   $(window).bind('storage', function(tar) {
-    console.log("FIRE")
-     if (tar.originalEvent.key == 'cart') {
+    console.log("FIRE");
 
-        // обновить список выведенной корзины
-        // формируем новый список в HTML
-        let new_cart_list = '';
-        $.each(tar.originalEvent.newValue, 
-          /*function(i, value) {
-           var box_title = data.boxes[ value[0] ].name;
-           var { months } = data.boxes[ value[0] ].subscriptions[ value[1] ];
-           let lenght = '--';
-           switch (months) {
-              case 1:
-                 lenght = '1 месяц';
-                break;
-              case 3:
-                 lenght = '3 месяца';
-                break;
-              case 6:
-                 lenght = '6 месяцев';
-                break;
-           }
+    //  if (tar.originalEvent.key == 'cart') {
 
-           var element = `<p> Hello </p> \
-            <p>` + box_title + `</p> \
-            <div class="user_choices"> \
-            <p class="choice_name">Подписка:</p> \
-            <p>` + lenght + `</p> `;
-                    
-           return new_cart_list += element;
-        }*/
-        );
+    //     // обновить список выведенной корзины
+    //     // формируем новый список в HTML
+    //     let new_cart_list = '';
+    //     $.each(tar.originalEvent.newValue, function(i, value) {
+    //       new_cart_list = createCart(i, value);           
+    //     });
 
-        // пихаем в список новый HTML
-        $('.cart_list_class').html( new_cart_list );
+    //     // пихаем в список новый HTML
+    //     $('.cart_list_class').html( new_cart_list );
 
 
       
 
-        // запрос новой итоговой цены
+    //     // запрос новой итоговой цены
 
-       /* return $.post('https://domain.com/link-to-request', tar.originalEvent.newValue, msg => {
-           console.log(msg);
-           // вывод общей цены
-           return $('.total_price').html( msg.total_price );
-        });*/
-      }
+    //    /* return $.post('https://domain.com/link-to-request', tar.originalEvent.newValue, msg => {
+    //        console.log(msg);
+    //        // вывод общей цены
+    //        return $('.total_price').html( msg.total_price );
+    //     });*/
+    //   }
   });
 
-});
 
 
 
+
+ 
