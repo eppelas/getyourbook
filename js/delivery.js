84badbdd29903b014226130ca10a0aa72393f6d9
Yaroslav Matveychuk <yma-extern@marketlogicsoftware.com>
2019-04-24 21:09:07 +0300

    Revert "Final Version"
    This reverts commit 3877cc5c78238832984bd507afee55e3a1c372c4, reversing
    changes made to 406ad9932e1858d59e41c9179b2d3e0fd15003c9.

--- js/delivery.js
+++ js/delivery.js
@@ -14,10 +14,7 @@ console.log(type);
 
 
 	$('#btn_delivery').click(function() {
-<<<<<<< HEAD
 		console.log('Hello');
-=======
->>>>>>> 18b26f7f04f57516388a34567cb0674b5cb84e4c
 		$('#faq_delivery').addClass('show');
 		$('.delivery').removeClass('collapsed');
 	} );
